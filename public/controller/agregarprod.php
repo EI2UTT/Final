145026356d703a5b5c0c26daec6aec8707ccb039
Evelyn Dominguez Gonzalez <20170039@uttcampus.edu.mx>
2021-08-11 20:55:10 -0500

    php agregar p o s

--- public/controller/agregarprod.php
+++ public/controller/agregarprod.php
@@ -4,18 +4,36 @@
     
     use Exception;
     
-
    try{
         require("../../vendor/autoload.php");
-  
-        extract($_POST);
-        $p = new DiseñoProducto();
-        $p->Nombre=$Nombre;
-        $p->Descripcion=$Descripcion;
-        $p->Foto=$Foto;
-        $p->Nota=$Nota;
-        $p->SERVICIO=$SERVICIO;
-        $p->EVENTO=$EVENTO;
+        if(isset($_POST)){
+          extract($_POST);
+          $resultado="";
+          $p = new DisenoProducto();
+          $p->fotodispro=$_FILES['imagen'] ['name'];
+            $tmp_name =$_FILES['imagen'] ['tmp_name'];
+            $error =$_FILES['imagen'] ['error'];
+            $size =$_FILES['imagen'] ['size'];
+            $max_size = 1024 * 1024 * 1;
+            $type =$_FILES['imagen'] ['type'];
+          $p->nombredispro=$nombredispro;
+          $p->descripcion=$descripcion;
+          $p->producto=$producto;
+          if($error){
+            $resultado="esta mal";
+          }
+          else if($size>$max_size){
+            $resultado="esta muy grande";
+          }
+          else if($type !='image/jpg' && $type !='image/png' && $type !='image/gif'){
+            $resultado="ta bien";
+          }
+          else{
+            $ruta=__DIR__ .'\imagenes\\' .$_FILES['imagen'] ['name'];
+            move_uploaded_file($tmp_name,$ruta);
+            $resultado="exito";
+          }
+        }
         $p->save();
         
       echo  json_encode( $p->all());
@@ -23,7 +41,18 @@
     } catch (Exception $e) {
            echo($e->getMessage());
     }
-//
+
+
+
+
+
+
+
+       
+       
+       
+       
+       
 
 
 
--- public/controller/agregarserv.php
+++ public/controller/agregarserv.php
@@ -4,25 +4,47 @@
     
     use Exception;
     
-
    try{
         require("../../vendor/autoload.php");
-  
-        extract($_POST);
-        $s = new DiseñoServicio();
+        if(isset($_POST)){
+          extract($_POST);
+          $resultado="";
+        $s = new DisenoServicio();
+        $s->fotodisser=$_FILES['file'] ['name'];
+          $tmp_name =$_FILES['file'] ['tmp_name'];
+          $error =$_FILES['file'] ['error'];
+          $size =$_FILES['file'] ['size'];
+          $max_size = 1024 * 1024 * 1;
+          $type =$_FILES['file'] ['type'];
         $s->nombredisser=$nombredisser;
         $s->descripcion=$descripcion;
-        $s->fotodisser=$fotodisser;
         $s->servicio=$servicio;
         $s->cantidadpersonas=$cantidadpersonas;
+          if($error){
+            $resultado="esta mal";
+          }
+          else if($size>$max_size){
+            $resultado="esta muy grande";
+          }
+          else if($type !='image/jpg' && $type !='image/png' && $type !='image/gif'){
+            $resultado="ta bien";
+          }
+          else{
+            $ruta=__DIR__ .'\imagenes\\' .$_FILES['file'] ['name'];
+            move_uploaded_file($tmp_name,$ruta);
+            $resultado="exito";
+          }
+        }
         $s->save();
-      
+        
       echo  json_encode( $s->all());
     
     } catch (Exception $e) {
            echo($e->getMessage());
     }
-    
+
+
+
 
 
 
